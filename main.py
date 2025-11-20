2fec03916b8634de04b9f3f155480bece1df696c
Aakash Pullat <aakashpullat.offical@gmail.com>
2025-11-20 22:14:44 +0530

    final commit

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-from stats import count_words,get_word_freq,get_sorted_char_freq
+from stats import count_words,get_sorted_char_freq
+import sys
 
 def get_book_text(path_to_file):
     with open(path_to_file,"r",encoding = "utf-8-sig") as f:
         file_contents = f.read()
         return file_contents
-
-path_to_file = "./books/frankenstein.txt"
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+path_to_file = sys.argv[1]
 text = get_book_text(path_to_file)
+count = count_words(text)
 sorted_char_freq = get_sorted_char_freq(text)
-print(sorted_char_freq)
+print(f"Found {count} total words")
+for i in sorted_char_freq:
+    print(f"{i["char"]}: {i["num"]}",end = "\n")
--- stats.py
+++ stats.py
@@ -6,16 +6,24 @@ def count_words(text):
 def get_char_freq(text):
     char_freq = {}
     for char in text:
+        if not(char.isalpha()):
+            continue
         char = char.lower()
-        if char in char_fre:
+        if char in char_freq:
             char_freq[char] += 1
         else:
             char_freq[char] = 1
     return char_freq
 
-def sorted_char_freq(text):
-    char_freq_list = []
-    for char in text:
-        char = char.lower()
-        if char_freq[{"char":char}]:
+def helper_sort(char_list):
+    return char_list["num"]
 
+def get_sorted_char_freq(text):
+    text = text.lower()
+    temp_list = get_char_freq(text)
+    sorted_char_list = []
+    for char in temp_list:
+        char_freq = {"char":char,"num":temp_list[char]}
+        sorted_char_list.append(char_freq)
+    sorted_char_list.sort(reverse=True, key=helper_sort)
+    return sorted_char_list
